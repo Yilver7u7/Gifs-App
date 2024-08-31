2234e2e5675d02cb9bc32145be1b3807132a6a4e
Yilver7u7 <yilversgg@gmail.com>
2024-08-31 18:42:20 -0500

    Add functionality @ViewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
-  styleUrl: './search-box.component.css'
+
 })
 export class SearchBoxComponent {
 
 
-  
+@ViewChild('textoInput')
+public tagInput!: ElementRef<HTMLInputElement>;
+
+  tagSearch(): void {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log( {newTag} );
+  }
+
+
 }
